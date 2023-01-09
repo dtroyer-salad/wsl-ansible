055227a404089e2a9fcb7657304be9464024a870
Dean Troyer <dean.troyer@salad.com>
2023-01-09 16:46:56 -0600

    Add vim to base install
    Signed-off-by: Dean Troyer <dean.troyer@salad.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,11 +30,11 @@ if [[ -r /etc/os-release ]]; then
       # Debian/Ubuntu
       sudo apt-get update
       sudo apt-get -y upgrade
-      sudo apt-get -y install ansible curl git gpg make screen wget
+      sudo apt-get -y install ansible curl git gpg make screen vim wget
       ;;
     fedora)
       # Fedora/RHEL/OEL
-      sudo dnf -y install ansible-core curl git gpg make screen wget
+      sudo dnf -y install ansible-core curl git gpg make screen vim wget
       ansible-galaxy collection install ansible.posix chocolatey.chocolatey community.crypto
       ;;
   esac
